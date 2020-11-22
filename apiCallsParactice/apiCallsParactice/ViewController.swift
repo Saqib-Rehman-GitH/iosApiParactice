2cb1672ccee4f4726d1a1a3c3a84a7ce0e3cd3ac
Saqib Rehman <saqibrehman@Saqibs-Mac.local>
2020-11-22 02:16:12 -0800

    Empty Project

---
+++ apiCallsParactice/apiCallsParactice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apiCallsParactice
+//
+//  Created by Saqib Rehman on 22/11/2020.
+//  Copyright © 2020 Saqib Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
